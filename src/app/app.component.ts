75069a4813086ac780580cc0dfef7d46b75cbce9
gerbermichi <gerbermichi@me.com>
2019-05-17 14:46:28 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {forkJoin, Subject, timer} from 'rxjs';
-import {debounceTime, mapTo, mergeMap} from 'rxjs/operators';
+import {forkJoin, fromEvent, Subject, timer} from 'rxjs';
+import {debounceTime, map, mapTo, mergeMap, pairwise} from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -33,9 +33,16 @@ export class AppComponent implements OnInit{
 
       const fakeGet = timer(1000).pipe(mapTo({id: 1}));
 
-      fakeGet.pipe(mergeMap(result => timer(1000).pipe(mapTo({id: result.id + 1}))))
-          .subscribe(res => console.log(res))
+      fakeGet.pipe(mergeMap(result => timer(1000).pipe(mapTo({foo: result.id + 1}))))
+          .subscribe(res => console.log("mergeMap", res));
+
+      //pairwise
+      fromEvent(document, 'mousemove')
+          .pipe(map((e: MouseEvent) => e.x))
+          .pipe(pairwise())
+          .subscribe(pair => console.log(pair[1] - pair[0])); 
   }
+
 
   input(event:Event){
     this.searchInput.next(event.target.value);
